17be992757a375bcbb07f32f7420cc2e2eb2fdb3
SamM35 <63260700+SamM35@users.noreply.github.com>
2020-05-05 18:48:13 -0400

    Add files via upload
    aaaaaaaaAAAAAAAAAA

--- Project/Main.js
+++ Project/Main.js
@@ -11,9 +11,9 @@ animate();
 
 function init() {
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 500 );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.z = 150;
-    camera.position.y= -40;
+    camera.position.y= -60;
 
     scene = new THREE.Scene();
 
@@ -27,8 +27,8 @@ function init() {
     scene.add( ambientLight );
 
     // Pyramid Code
-    const radius = 64;
-    const height = 80;
+    const radius = 80;
+    const height = 100;
 
     var pMat =
         [
@@ -43,7 +43,7 @@ function init() {
     const pyramid_shape = new THREE.CylinderGeometry(0, radius, height, 4, 1 );
     const pMaterial = new THREE.MeshFaceMaterial( pMat);
     const Pyramid = new THREE.Mesh( pyramid_shape, pMaterial );
-    Pyramid.position.set(0,-32,0);
+    Pyramid.position.set(0,-20,0);
     scene.add( Pyramid );
 
     var skyMaterials =
@@ -56,7 +56,7 @@ function init() {
             new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "frontside.png"), side: THREE.DoubleSide}) // front
         ]
 
-    const skybox = new THREE.BoxGeometry( 350, 350, 350 );
+    const skybox = new THREE.BoxGeometry( 400, 400, 400 );
     const sky_material = new THREE.MeshFaceMaterial( skyMaterials );
     const sky = new THREE.Mesh( skybox, sky_material );
     sky.position.set(0, 125, 0);
@@ -74,7 +74,7 @@ function init() {
     var sign_stand = new THREE.CylinderGeometry( .5, .5, 4, 3.2 );
     var sign_material = new THREE.MeshBasicMaterial( {color: 0x4a3200} );
     var stand = new THREE.Mesh( sign_stand, sign_material );
-    stand.position.set(-25, -45, 120);
+    stand.position.set(-25, -65, 120);
     scene.add( stand );
 
     var sign_materials =
@@ -84,15 +84,81 @@ function init() {
             new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "wood.jpg"), side: THREE.DoubleSide}),// right side
             new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "wood.jpg"), side: THREE.DoubleSide}),// right side
             new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "wood.jpg"), side: THREE.DoubleSide}),// right side
-            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "wood.jpg"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "wood.jpg"), side: THREE.DoubleSide})// right side
         ]
 
     const sign_board = new THREE.BoxGeometry( 8, 3, .5 );
     const sign_mat = new THREE.MeshFaceMaterial( sign_materials );
     const sign = new THREE.Mesh( sign_board, sign_mat );
-    sign.position.set(-25, -41.5, 120);
+    sign.position.set(-25, -61.5, 120);
     sign.rotation.set(0, Math.PI*2.2, 0);
     scene.add( sign );
+
+
+
+
+    var water_materials =
+        [
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "water.jpg"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "water.jpg"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "water.jpg"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "water.jpg"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "water.jpg"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "water.jpg"), side: THREE.DoubleSide})
+        ]
+
+    const circle = new THREE.CylinderGeometry( 20, 20, .5, 132  );
+    const water_mat = new THREE.MeshFaceMaterial( water_materials );
+    const oasis = new THREE.Mesh( circle, water_mat );
+    oasis.position.set(50, -75 , 115);
+    scene.add( oasis );
+
+
+    var palm_materials =
+        [
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "Palm.png"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "Palm.png"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "Palm.png"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "Palm.png"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "Palm.png"), side: THREE.DoubleSide}),// right side
+            new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "Palm.png"), side: THREE.DoubleSide})
+        ]
+
+    const tree = new THREE.CylinderGeometry( .5, 3, 64, 3.2 );
+    const tree_mat = new THREE.MeshFaceMaterial( palm_materials);
+    const palm = new THREE.Mesh( tree, tree_mat );
+    palm.position.set(75, -70 , 115);
+    scene.add( palm );
+
+    var geometry = new THREE.ConeBufferGeometry( 4, 15, 2 );
+    var material = new THREE.MeshBasicMaterial( {color: 0x45c400} );
+    var cone = new THREE.Mesh( geometry, material );
+    cone.position.set(73, -40 , 108);
+    cone.rotation.set(0,5,5);
+    scene.add( cone );
+
+    var geometry2 = new THREE.ConeBufferGeometry( 4, 15, 2 );
+    var material2 = new THREE.MeshBasicMaterial( {color: 0x398a0e} );
+    var cone2 = new THREE.Mesh( geometry2, material2 );
+    cone2.position.set(81.5, -40 , 110.5);
+    cone2.rotation.set(0,10,5);
+    scene.add( cone2 );
+
+    var geometry3 = new THREE.ConeBufferGeometry( 4, 15, 2 );
+    var material3 = new THREE.MeshBasicMaterial( {color: 0x69bf39} );
+    var cone3 = new THREE.Mesh( geometry3, material3 );
+    cone3.position.set(80, -40 , 119);
+    cone3.rotation.set(0,15,5);
+    scene.add( cone3 );
+
+
+
+
+
+
+
+
+//create material, color, or texture
 
 
 
@@ -146,6 +212,8 @@ function animate() {
 
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
+
+
 
 }
 
